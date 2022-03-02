4548a297d5f07559f09972ad5adf94014ed4b18e
Luka Weis <lukat.weis@gmail.com>
2022-03-02 23:42:21 +0100

    added GetLocalDrivesFreeSpace and GetTopTablesBySize

--- SqlModule/1.0.0.0/SqlModule.psm1
+++ SqlModule/1.0.0.0/SqlModule.psm1
@@ -451,3 +451,54 @@ function GetDbNettoSize([string]$DatabaseServerInstance, [string]$DatabaseName, 
 
     return $dbSize
 }
+
+
+function GetTopTablesBySize([int]$TableCount, [string]$DatabaseServerInstance, [string]$DatabaseName, [PSCredential]$SQLCredential = $null) {
+
+    $largeTablesQuery = "
+
+        SELECT TOP ($($TableCount))
+            t.name AS TableName,
+            s.name AS SchemaName,
+            p.rows AS RowCounts,
+            SUM(a.total_pages) * 8 AS TotalSpaceKB, 
+            SUM(a.used_pages) * 8 AS UsedSpaceKB, 
+            (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceKB
+        FROM 
+            sys.tables t
+            INNER JOIN sys.indexes i ON t.object_id = i.object_id
+            INNER JOIN sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+            INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+            LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
+        WHERE 
+            t.name NOT LIKE 'dt%' 
+            AND t.is_ms_shipped = 0
+            AND i.object_id > 255 
+        GROUP BY 
+            t.name, s.name, p.rows
+        ORDER BY 
+            UsedSpaceKB DESC
+    "
+
+    return (SqlModule\ExecuteQuery -Query $largeTablesQuery -DatabaseServerInstance $DatabaseServerInstance -DatabaseName $DatabaseName -SQLCredential $SQLCredential)
+}
+
+
+function GetLocalDrivesFreeSpace([string]$DatabaseServerInstance, [PSCredential]$SQLCredential = $null) {
+
+    $dbDriveSpaceQuery = "
+
+        SELECT 
+            DISTINCT dovs.logical_volume_name AS LogicalName,
+            SUBSTRING(dovs.volume_mount_point,1,1) AS Drive,
+            CONVERT(INT,dovs.available_bytes/1048576.0) AS FreeSpaceInMB
+        FROM 
+            sys.master_files mf
+        CROSS APPLY 
+            sys.dm_os_volume_stats(mf.database_id, mf.FILE_ID) dovs
+        ORDER BY 
+            FreeSpaceInMB ASC
+    "
+
+    return (ExecuteQuery -DatabaseServerInstance $DatabaseServerInstance -SQLCredential $SQLCredential -DatabaseName 'master' -Query $dbDriveSpaceQuery)
+}
